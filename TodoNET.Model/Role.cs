fa69361de5063ff5b61b3d1e9ed19fd9999eeb4e
Chris Tattum <christattum@gmail.com>
2011-03-18 17:54:54 +0000

    Added entities and mapping files for User and Role

---
+++ TodoNET.Model/Role.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AperioReal.Membership
+{
+    public class Role
+    {
+        public virtual ICollection<User> Users { get; protected set; }
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+
+        public Role()
+        {
+            Users = new HashSet<User>();
+        }
+
+
+    }
+}
---
+++ TodoNET.Model/User.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AperioReal.Membership
+{
+    public class User
+    {
+        public virtual ICollection<Role> Roles { get; protected set; }
+
+        public virtual int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a user name")] [StringLength(50)]
+        public virtual string UserName { get; set; }
+        
+        [Required(ErrorMessage= "Please enter a valid email address")] [StringLength(100)]
+        public virtual string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password")] [StringLength(50)]
+        public virtual string Password { get; set; }
+        
+        public virtual bool LockedOut { get; set; }
+
+        public virtual bool IsAdmin()
+        {
+            // Admin user is first added user, so has Id of 1
+            return (Id == 1);
+        }
+
+        public User()
+        {
+            Roles = new HashSet<Role>();
+        }
+
+    }
+}
